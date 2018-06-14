f97c2b02bc50dad683d6e505c6f040c2852d99e6
lucianxiang <794811154@qq.com>
2018-06-14 17:15:48 +0800

    LX0614commit

--- src/views/movie/VMovieCheckTable.vue
+++ src/views/movie/VMovieCheckTable.vue
@@ -606,12 +606,24 @@
         this.$store.commit('setTranstionFalse');
       },
       addSubmit() {
-        let date = new Date();
-        let day = date.getDay();
-        let newDate = date.getFullYear() + "/" + date.getMonth() + "/" + day + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+        Date.prototype.Format = function (fmt) {
+          var o = {
+            "M+": this.getMonth() + 1, //月份
+            "d+": this.getDate(), //日
+            "h+": this.getHours(), //小时
+            "m+": this.getMinutes(), //分
+            "s+": this.getSeconds(), //秒
+            "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+            "S": this.getMilliseconds() //毫秒
+          };
+          if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+          for (var k in o)
+            if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+          return fmt;
+        };
+        this.addOptions.data.vf_ve_Content.vf_vo_CreateTime = new Date().Format("yyyy/MM/dd hh:mm:ss");
         this.addOptions.data.vf_ve_Content.vf_te_IDs=this.categoriesName.join(",");
         this.addOptions.data.vf_ve_Type=this.parentTypeId.join(",");
-        this.addOptions.data.vf_ve_Content.vf_vo_CreateTime = newDate;
         this.addOptions.data.vf_ve_Content.vf_vo_AuthorID = "1";//
         this.$store.dispatch("addVMovieCheckTable", this.addOptions)
           .then((suc) => {
